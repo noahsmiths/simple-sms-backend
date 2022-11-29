ffc78849665eb8e5dec73a8dbb7bbe420490792d
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2022-11-29 17:18:31 -0500

    updated to use mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 const VenmoAPI = require('../venmo/VenmoAPI');
+const { MongoClient } = require("mongodb");
 const { Server } = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
+
+const venmo = new VenmoAPI(process.env.VENMO_BUSINESS_TOKEN, process.env.VENMO_BUSINESS_ID);
+const client = new MongoClient(process.env.MONGO_URI);
+
+const orderDB = client.db('orders');
+const awaitingPaymentCollection = orderDB.collection('awaitingPayment');
+const awaitingNumberCollection = orderDB.collection('awaitingNumber');
+const completedOrderCollection = orderDB.collection('completed');
+
+// const pendingPaymentOrders = new Set();
+// const ordersWaitingForNumbers = new Map();
+// const completedOrders = new Map();
+
 const io = new Server({
     cors: {
         origin: "http://localhost:8080",
@@ -9,20 +23,19 @@ const io = new Server({
     }
 });
 
-const venmo = new VenmoAPI(process.env.VENMO_BUSINESS_TOKEN, process.env.VENMO_BUSINESS_ID);
-const pendingPaymentOrders = new Set();
-const ordersWaitingForNumbers = new Map();
-const completedOrders = new Map();
-
 io.on('connection', (socket) => {
-    socket.on('create-order', () => {
-        const orderId = uuidv4();
+    socket.on('create-order', async () => {
+        try {
+            const orderId = uuidv4();
 
-        console.log(orderId);
-
-        socket.join(orderId);
-        pendingPaymentOrders.add(orderId);
-        socket.emit('order-created', { orderId });
+            console.log(orderId);
+    
+            await awaitingPaymentCollection.insertOne({ orderId: orderId });
+            socket.join(orderId);
+            socket.emit('order-created', { orderId });
+        } catch (err) {
+            socket.emit('error-creating-order');
+        }
     });
 });
 
@@ -35,24 +48,28 @@ venmo.on('new-transaction', async (tx) => {
 
         if (parsedTx.length < 3) {
             console.log("here");
-            venmo.refundTransaction(tx.id, tx.amount);
+            await venmo.refundTransaction(tx.id, tx.amount);
+            await awaitingPaymentCollection.deleteOne({ orderId: orderId });
             return;
         }
 
         let service = parsedTx[1];
         orderId = parsedTx[2];
 
-        if (!pendingPaymentOrders.has(orderId) && ordersWaitingForNumbers.has(orderId) && completedOrders.has(orderId)) {
+        if (!(await orderIdIsValid(orderId))) { // Order is invalid
+
             console.log("here 2");
             io.to(orderId).emit('invalid-session');
-            await venmo.refundTransaction();
+            await venmo.refundTransaction(tx.id, tx.amount);
+            await awaitingPaymentCollection.deleteOne({ orderId: orderId });
             return;
         }
 
         if (/* Lookup service and check that price paid is valid here */parseInt(tx.amount) !== 20) {
             console.log("here 3");
             io.to(orderId).emit('invalid-payment');
-            await venmo.refundTransaction();
+            await venmo.refundTransaction(tx.id, tx.amount);
+            await awaitingPaymentCollection.deleteOne({ orderId: orderId });
             return;
         }
 
@@ -64,8 +81,11 @@ venmo.on('new-transaction', async (tx) => {
             customerVenmoId: tx.customerVenmoId
         }
 
-        pendingPaymentOrders.delete(orderId);
-        ordersWaitingForNumbers.set(orderId, order);
+        // pendingPaymentOrders.delete(orderId);
+        // ordersWaitingForNumbers.set(orderId, order);
+
+        await awaitingPaymentCollection.deleteOne({ orderId: orderId });
+        await awaitingNumberCollection.insertOne(order);
 
         io.to(orderId).emit('order-confirmed');
     } catch (err) {
@@ -90,8 +110,23 @@ venmo.on('new-transaction', async (tx) => {
     // }
 });
 
+const orderIdIsValid = async (id) => {
+    try {
+        let isAwaitingPayment = (await awaitingPaymentCollection.findOne({ orderId: id })) !== null;
+        let notUsed = (await awaitingNumberCollection.findOne({ orderId: id })) === null && (await completedOrderCollection.findOne({ orderId: id })) === null;
+
+        return isAwaitingPayment && notUsed;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
+
 venmo.on('error', (err) => {
     console.log(err);
 });
 
-io.listen(3000);
+const port = 3000;
+io.listen(port);
+
+console.log(`Listening on port *:${port}`);
