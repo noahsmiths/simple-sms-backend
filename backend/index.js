7f02b10660f4fc73a67da3417b475ce94cc554a7
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-02-23 00:33:13 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -267,7 +267,7 @@ const getNumberForOrder = async (orderId, service) => {
     // Send webhook to discord 
     let updatedNumber = `+${order.number.substring(0, 1)} (${order.number.substring(1, 4)}) ${order.number.substring(4, 7)}-${order.number.substring(7)}`;
 
-    hook.success(`Order Confirmed ${orderId}`, `📱 ${updatedNumber}`, `🧪 ${service}`);
+    hook.success(`Order Confirmed ${orderId}`, `Number: ${updatedNumber}`, `Service: ${service}`);
 
 
     await awaitingFirstTextCollection.insertOne(order);
